514673d928444b98b4f48f5bc3ca538f91aa3f8a
Gökhan İpek <gipek03@gmail.com>
2018-09-04 17:58:55 +0300

    secondcommit

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,16 +1,51 @@
-import { AngularFireDatabase } from 'angularfire2/database';
+import { ChatMessage } from './../models/chat-message.model';
 import { Injectable } from '@angular/core';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import { ChatMessage } from './models/chat-message.model';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import * as firebase from 'firebase/app';
+
+import { ChatMessage } from '../models/chat-message.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  chatMessages: FirebaseListObservable<ChatMessage[]>;
+  chatMessage: ChatMessage;
+  userName: Observable<string>;
+  user: any; 
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) { 
+    this.afAuth.authState.subscribe(auth => {
+      if( auth !== undefined && auth !== null){
+        this.user = auth;
+      }
+    })
+  }
+
+  sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    const email = this.user.email;
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      userName: this.userName,
+      email: email
+    })
+  }
+  getTimeStamp(){
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' + ( now.getMonth() + 1 )+ '/' + now.getDate();
+    const time = now.getUTCHours() + '/' + now.getUTCMinutes() + '/' + now.getUTCSeconds();
+  }
+
+  getMessages(){
+    
+  }
 }
